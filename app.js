5b8b758adcc2048b57bed999d8c948f89f4ec770
Yanchen <fengyc2018@gmail.com>
2018-10-22 23:08:17 -0400

    Adjust the if production condition

--- app.js
+++ app.js
@@ -33,13 +33,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  })
-}
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -77,6 +71,13 @@ app.use('/api/promotions', promosRouter);
 app.use('/api/leaders', leadersRouter);
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/comments', commentRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
